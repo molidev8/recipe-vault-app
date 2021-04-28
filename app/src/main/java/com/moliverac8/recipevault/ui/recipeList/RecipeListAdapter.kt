e5aa3087d939adfa3a1924b2b9dce0f68fe9f0e0
moliv <moliverac8@gmail.com>
2021-04-28 09:14:20 +0200

    Arreglando bugs visuales

--- app/src/main/java/com/moliverac8/recipevault/ui/recipeList/RecipeListAdapter.kt
+++ app/src/main/java/com/moliverac8/recipevault/ui/recipeList/RecipeListAdapter.kt
@@ -1,5 +1,6 @@
 package com.moliverac8.recipevault.ui.recipeList
 
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
